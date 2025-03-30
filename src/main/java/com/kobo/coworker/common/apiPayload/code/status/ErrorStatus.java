db79ec5e46bd3ef6972cabff0198ffcadc4f8680
Park, seun <seunpark04@gmail.com>
2025-03-30 17:10:14 +0900

    Feat: 기본적인 응답구조 통일 ApiResponse 추가

---
+++ src/main/java/com/kobo/coworker/common/apiPayload/code/status/ErrorStatus.java
@@ -0,0 +1,37 @@
+package com.kobo.coworker.common.apiPayload.code.status;
+
+import com.kobo.coworker.common.apiPayload.code.BaseErrorCode;
+import com.kobo.coworker.common.apiPayload.code.ErrorReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class ErrorStatus implements BaseErrorCode {
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDto getReason() {
+        return ErrorReasonDto.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDto getReasonHttpStatus() {
+        return ErrorReasonDto.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .build()
+                ;
+    }
+
+}
---
+++ src/main/java/com/kobo/coworker/common/apiPayload/exception/GeneralException.java
@@ -0,0 +1,22 @@
+package com.kobo.coworker.common.apiPayload.exception;
+
+import com.kobo.coworker.common.apiPayload.code.BaseErrorCode;
+import com.kobo.coworker.common.apiPayload.code.ErrorReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDto getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDto getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+
+}
