69083bd9054a5f91d71fa2db704144dc7e299fe1
Park, seun <seunpark04@gmail.com>
2025-04-14 19:21:28 +0900

    Test : 파일 경로 중복 시 DOCUMENT_ALREADY_EXISTS 예외가 발생한다.

--- src/test/java/com/kobo/coworker/document/service/DocumentServiceTest.java
+++ src/test/java/com/kobo/coworker/document/service/DocumentServiceTest.java
@@ -17,14 +17,19 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.multipart.MultipartFile;
 import java.security.Principal;
+import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 public class DocumentServiceTest {
 
     @Mock
     private UserService userService;
+
+    @Mock
+    private DocumentRepository repository;
 
 
     @InjectMocks
@@ -58,6 +63,29 @@ public class DocumentServiceTest {
                         assertThat(ex.getCode()).isEqualTo(ErrorStatus.DOCUMENT_INVALID_FILE_TYPE);
                     });
         }
+
+        @Test
+        @DisplayName("파일 경로 중복 시 DOCUMENT_ALREADY_EXISTS 예외가 발생한다.")
+        void throwDocumentAlreadyExists_WithAlreadyExists() {
+            String sampleFileUrl = sampleDocument.getFileUrl();
+            when(repository.findByFileUrl(sampleFileUrl)).thenReturn(Optional.empty());
+
+            assertThatThrownBy(() -> service.validateDocumentFileUrlUniqueness(sampleFileUrl))
+                    .isInstanceOf(GeneralException.class)
+                    .satisfies(e -> {
+                        GeneralException ex = (GeneralException) e;
+                        assertThat(ex.getCode()).isEqualTo(ErrorStatus.DOCUMENT_ALREADY_EXISTS);
+                    });
+        }
+
+//        @Test
+//        @DisplayName("유효한 문서 타입일 경우 예외 없이 업로드가 진행된다.")
+//        void shouldUploadFile_WhenFileTypeIsValid() {
+//            Principal mockPrincipal = () -> "testUser";
+//            MultipartFile sampleDocumentFile = sampleMultipartFile;
+//
+//
+//        }
 
     }
 }
