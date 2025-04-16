995dcbbb8d7dfd68d7f1608935dfc06a9559b90a
Park, seun <seunpark04@gmail.com>
2025-04-16 17:18:35 +0900

    Fix: Jackson 역직렬화를 위해 QuestionReqDto에 어노테이션 추가

--- src/main/java/com/kobo/coworker/question/dto/QuestionReqDto.java
+++ src/main/java/com/kobo/coworker/question/dto/QuestionReqDto.java
@@ -1,12 +1,16 @@
 package com.kobo.coworker.question.dto;
 
 import com.kobo.coworker.document.domain.Document;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuestionReqDto {
-
     private Document document;
     private String content;
-
 }
